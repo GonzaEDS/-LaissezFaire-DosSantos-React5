5b042f30cadcaf223170a1136bc747cb5b237191
Gonzalo Dos Santos <die.zaubernacht@gmail.com>
2022-07-08 18:08:58 -0300

    Updates

---
+++ precache-manifest.c0ad928e50e6d509a7ec9fe794a0aeb8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "7aaf4695f8c007d6a4c4",
+    "url": "/LaissezFaire-DosSantos-React4/static/css/main.236e33fb.chunk.css"
+  },
+  {
+    "revision": "7aaf4695f8c007d6a4c4",
+    "url": "/LaissezFaire-DosSantos-React4/static/js/main.68e334bc.chunk.js"
+  },
+  {
+    "revision": "658964a865f8bc6f1740",
+    "url": "/LaissezFaire-DosSantos-React4/static/js/runtime~main.5b4fa1ee.js"
+  },
+  {
+    "revision": "ddec33a8187cf398706d",
+    "url": "/LaissezFaire-DosSantos-React4/static/css/2.2a6720e4.chunk.css"
+  },
+  {
+    "revision": "ddec33a8187cf398706d",
+    "url": "/LaissezFaire-DosSantos-React4/static/js/2.57b58bac.chunk.js"
+  },
+  {
+    "revision": "1e38ecc495746bd18357",
+    "url": "/LaissezFaire-DosSantos-React4/static/js/3.12bf67e3.chunk.js"
+  },
+  {
+    "revision": "a6a4862c8f0a585c6c1f6d5aa07b761c",
+    "url": "/LaissezFaire-DosSantos-React4/static/media/bootstrap-icons.a6a4862c.woff"
+  },
+  {
+    "revision": "b57a58905370284f1499d1a35f5d2c96",
+    "url": "/LaissezFaire-DosSantos-React4/static/media/bootstrap-icons.b57a5890.woff2"
+  },
+  {
+    "revision": "7a7c72936965adf2a9a1d81e3d9dba53",
+    "url": "/LaissezFaire-DosSantos-React4/index.html"
+  }
+];
